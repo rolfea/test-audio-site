551b1a337c8d74747bc2c0c31b8ac3f82ae8232b
Alex Rolfe <alex.rolfe27@gmail.com>
2020-05-05 07:25:32 -0500

    Refactors into smaller functions

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 //import { wavetable } from './wavetable';
-let audioContext;
+
+// globals
+let AUDIO_CONTEXT;
+const SWEEP_LENGTH = 2;
+let ATTACK_TIME = 0.2;
+let RELEASE_TIME = 0.5;
+
 
 window.onload = () => {  
   init();
@@ -7,12 +13,22 @@ window.onload = () => {
 
 function init() {
   document.querySelector('#consent').addEventListener('click', () => {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    audioContext = new AudioContext();
-    revealConsole(); 
-    initializeButton("A", 440);
-    initializeButton("B", 493.88);   
+    createAudioContext();
+    initializeConsole();
   })  
+}
+
+function createAudioContext() {
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  AUDIO_CONTEXT = new AudioContext();
+}
+
+function initializeConsole() {
+  revealConsole();
+  initializeButton("A", 440);
+  initializeButton("B", 493.88);   
+  initializeSlider("attack");
+  initializeSlider("release");
 }
 
 function revealConsole() {
@@ -21,41 +37,44 @@ function revealConsole() {
 
 function initializeButton(btnId, hertz) {
   document.querySelector(`#${btnId}`).addEventListener('click', () => {
-    audioContext.resume().then( () => {
-      playSweep(audioContext, hertz);
-    });
+    playSweep(AUDIO_CONTEXT, hertz);    
   });
 }
 
-let sweepLength = 2;
+function initializeSlider(sliderSelector) {
+  const sliderElement = document.querySelector(`#${sliderSelector}`);
+  sliderElement.addEventListener('input', (event) => {
+    attackTime = Number(event.srcElement.value);
+  }, false);
+}
+
 function playSweep(ctx, hertz) {
   let wave = ctx.createPeriodicWave(wavetable.real, wavetable.imag);
 
-  let osc = ctx.createOscillator();
-  osc.setPeriodicWave(wave);
-  osc.frequency.value = hertz;
-
-  let sweepEnv = ctx.createGain();
-  sweepEnv.gain.cancelScheduledValues(ctx.currentTime);
-  // attack
-  sweepEnv.gain.linearRampToValueAtTime(1, ctx.currentTime + attackTime);
-  // release
-  sweepEnv.gain.linearRampToValueAtTime(0, ctx.currentTime + sweepLength - releaseTime);
-
+  let osc = generateOscillator(ctx, wave, hertz);
+  let sweepEnv = generateSweepEnv(ctx, ATTACK_TIME, RELEASE_TIME, SWEEP_LENGTH);
+  
   osc.connect(sweepEnv).connect(ctx.destination);
   osc.start();
-  osc.stop(ctx.currentTime + sweepLength);
+  osc.stop(ctx.currentTime + SWEEP_LENGTH);
 }
 
-let attackTime = 0.2;
-const attackControl = document.querySelector('#attack');
-attackControl.addEventListener('input', (event) => {
-  attackTime = Number(event.srcElement.value);
-}, false);
+function generateOscillator(ctx, wave, hertz) {
+  let osc = ctx.createOscillator();
+  osc.setPeriodicWave(wave)
+  osc.frequency.value = hertz;
 
-let releaseTime = 0.5;
-const releaseControl = document.querySelector('#release');
-releaseControl.addEventListener('input', () => {
-  releaseTime = Number(event.srcElement.value);
-}, false);
+  return osc;
+}
 
+function generateSweepEnv(ctx, attackTime, releaseTime, sweepLength) {
+  let env = ctx.createGain();
+
+  env.gain.cancelScheduledValues(ctx.currentTime);
+  // attack
+  env.gain.linearRampToValueAtTime(1, ctx.currentTime + attackTime);
+  // release
+  env.gain.linearRampToValueAtTime(0, ctx.currentTime + sweepLength - releaseTime);
+
+  return env;
+}
